5b531ec47aed721cade3aadc17e258d63f7f4f9a
Vlad Alive <vladalive@gmail.com>
2010-04-14 23:11:36 +0400

    Initial commit;

---
+++ lib/default.rb
@@ -0,0 +1,12 @@
+# All files in the 'lib' directory will be loaded
+# before nanoc starts compiling.
+include Nanoc3::Helpers::Blogging
+include Nanoc3::Helpers::Breadcrumbs
+include Nanoc3::Helpers::Capturing
+include Nanoc3::Helpers::Filtering
+include Nanoc3::Helpers::HTMLEscape
+include Nanoc3::Helpers::LinkTo
+include Nanoc3::Helpers::Rendering
+include Nanoc3::Helpers::Tagging
+include Nanoc3::Helpers::Text
+include Nanoc3::Helpers::XMLSitemap
